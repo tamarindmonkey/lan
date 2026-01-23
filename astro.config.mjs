c682c7e5cc61b0fa8afcdad52f284ce0486d4eb4
cram <34961432+tamarindmonkey@users.noreply.github.com>
2026-01-22 20:42:25 -0500

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+export default defineConfig({
+  site: 'https://tamarindmonkey.github.io',
+  base: '/lan',
+  integrations: [
+    starlight({
+      title: 'LAN/Services'
+    }),
+  ],
+});
